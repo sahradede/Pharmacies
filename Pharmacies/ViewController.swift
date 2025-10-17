cf91fa65827d5c4908f266731c35ad8f7a8cbd10
sahradede <ssahradede@gmail.com>
2025-10-17 23:38:47 +0300

    Initial Commit

---
+++ Pharmacies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pharmacies
+//
+//  Created by Sahra Dede on 12.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
