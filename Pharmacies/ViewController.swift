c5ab743c09a7522c26e0751a227942f6408723b4
sahradede <ssahradede@gmail.com>
2025-11-28 16:40:59 +0300

    storyboard

--- Pharmacies/ViewController.swift
+++ Pharmacies/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
